f3907d4be0356204b05e47bf2f16c3d5e081b38e
Willian Ribas <williann.dev@gmail.com>
2025-01-27 00:06:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
-  
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    base: "/siteos",
   },
   test: {
     globals: true,
